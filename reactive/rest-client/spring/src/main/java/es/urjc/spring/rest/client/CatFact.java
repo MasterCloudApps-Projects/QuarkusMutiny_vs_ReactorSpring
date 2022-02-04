00a4c52bd9b4f758246d7580bea2ef4ecf536260
izan.santana <ethanmmd@gmail.com>
2022-02-04 11:24:00 +0100

    Adds spring reactive rest to rest, some projects readme.md and updated parent README.md

---
+++ reactive/rest-client/spring/src/main/java/es/urjc/spring/rest/client/CatFact.java
@@ -0,0 +1,33 @@
+package es.urjc.spring.rest.client;
+
+public class CatFact {
+
+    private String fact;
+    private int length;
+
+    public CatFact(String fact, int length) {
+        this.fact = fact;
+        this.length = length;
+    }
+
+
+    public CatFact(String fact) {
+        this.fact = fact;
+    }
+
+    public String getFact() {
+        return fact;
+    }
+
+    public void setFact(String fact) {
+        this.fact = fact;
+    }
+
+    public int getLength() {
+        return length;
+    }
+
+    public void setLength(int length) {
+        this.length = length;
+    }
+}
